398dcd0dc8af4d0b0b68386a549133578cf540fc
Chris Donnelly <christopher.d.donnelly@gmail.com>
2018-02-06 07:07:56 -0800

    Add 'return self' to display_info method to allow chaining

--- Car.py
+++ Car.py
@@ -53,6 +53,7 @@ class Car(object):
         else:
             self.tax = 0.12
         self.display_all()
+        return self
     def display_all(self):
         print 'Price: ',self.price,'\nSpeed: ',self.speed,'\nFuel: ',self.fuel,'\nMileage: ',self.milage,'\nTax: ',self.tax,'\n'
         return self
